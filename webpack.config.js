9388bb6024bef0c6cada1144ecffe42b45051621
Tatsuya Yamamoto <yamamoto.tatsuya@fujixerox.co.jp>
2017-04-14 01:11:23 +0900

    add scritp for build

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    entry: "./src/Index.ts",
+    entry: "./src/index.ts",
     output: {
         path: path.resolve(__dirname, `dist`),
         filename: `kxing.js`,
