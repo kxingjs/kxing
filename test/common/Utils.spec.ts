5ceb12c2dcf99b3586ea3df2067c2bdbb562800a
Tatsuya Yamamoto <yamamoto.tatsuya@fujixerox.co.jp>
2017-04-15 19:35:29 +0900

    reformat test code

--- test/common/Utils.spec.ts
+++ test/common/Utils.spec.ts
@@ -1,29 +1,30 @@
 import {expect} from 'chai';
 import {arraycopy} from '../../src/common/Utils'
 
+describe("Utils", () => {
+    describe("#arraycopy()", () => {
+        it("should return arraycopy result same as Java", () => {
+            const src = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+            const dest = [10, 11, 12, 13, 14, 15, 16, 17, 18, 19];
+            const expectDest = [0, 1, 2, 3, 4, 15, 16, 17, 18, 19];
 
-describe("test: arraycopy()", () => {
-    it("should", () => {
-        const src = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-        const dest = [10, 11, 12, 13, 14, 15, 16, 17, 18, 19];
-        const expectDest = [0, 1, 2, 3, 4, 15, 16, 17, 18, 19];
+            arraycopy(src, 0, dest, 0, 5);
 
-        arraycopy(src, 0, dest, 0, 5);
+            dest.forEach(function (value, index) {
+                expect(value).to.be.equal(expectDest[index]);
+            })
+        });
 
-        dest.forEach(function (value, index) {
-            expect(value).to.be.equal(expectDest[index]);
-        })
-    });
+        it("should return arraycopy result same as Java", () => {
+            const src = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+            const dest = [10, 11, 12, 13, 14, 15, 16, 17, 18, 19];
+            const expectDest = [10, 11, 3, 4, 5, 6, 16, 17, 18, 19];
 
-    it("should", () => {
-        const src = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-        const dest = [10, 11, 12, 13, 14, 15, 16, 17, 18, 19];
-        const expectDest = [10, 11, 3, 4, 5, 6, 16, 17, 18, 19];
+            arraycopy(src, 3, dest, 2, 4);
 
-        arraycopy(src, 3, dest, 2, 4);
-
-        dest.forEach(function (value, index) {
-            expect(value).to.be.equal(expectDest[index]);
-        })
+            dest.forEach(function (value, index) {
+                expect(value).to.be.equal(expectDest[index]);
+            })
+        });
     });
 });
