5554f50a7f0f5c2745028617105b96401bdc5e49
Tatsuya Yamamoto <yamamoto.tatsuya@fujixerox.co.jp>
2017-04-17 21:20:39 +0900

    arrange and fix, can't decode Large image.

--- examples/index.js
+++ examples/index.js
@@ -7,23 +7,35 @@ const
     barcodeFormat = document.getElementById("barcodeFormat"),
     timestamp = document.getElementById("timestamp");
 
-image.onload = function () {
-    try {
-        const canvasElement = document.createElement('canvas');
-        const context = canvasElement.getContext('2d');
-        context.drawImage(image, 0, 0, image.width, image.height);
+image.onload = decodeWithKogera;
+image.src = filePath;
 
+function decodeWithKogera() {
+    try {
+        const context = createDrawnCanvasContext(image);
         const result = KXing.getReader().decode(context.getImageData(0, 0, image.width, image.height));
 
-        text.innerHTML = result.text;
-        rawBytes.innerHTML = result.rawBytes;
-        numBits.innerHTML = result.numBits;
-        barcodeFormat.innerHTML = result.barcodeFormat;
-        timestamp.innerHTML = result.timestamp;
-
+        setResult(result);
     } catch (e) {
         console.error('fail to decode.', e);
     }
-};
+}
 
-image.src = filePath;
+function createDrawnCanvasContext(image) {
+    const canvasElement = document.createElement('canvas');
+    canvasElement.width = image.width;
+    canvasElement.height = image.height;
+
+    const context = canvasElement.getContext('2d');
+    context.drawImage(image, 0, 0);
+
+    return context;
+}
+
+function setResult(result) {
+    text.innerHTML = result.text;
+    rawBytes.innerHTML = result.rawBytes;
+    numBits.innerHTML = result.numBits;
+    barcodeFormat.innerHTML = result.barcodeFormat;
+    timestamp.innerHTML = result.timestamp;
+}
