0886e20ba99182a16fdb1279dab7d8a573f85e42
Tatsuya Yamamoto <yamamoto.tatsuya@fujixerox.co.jp>
2017-06-11 16:07:53 +0900

    add exclude code on build.

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,7 @@ module.exports = {
         rules: [
             {
                 test: /\.tsx?$/,
+                exclude: /node_modules/,
                 use: [
                     {
                         loader: 'ts-loader'
