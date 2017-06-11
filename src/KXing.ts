0074f6d621bed66aa9ad4c93271845f0791dd47f
Tatsuya Yamamoto <yamamoto.tatsuya@fujixerox.co.jp>
2017-06-11 16:11:38 +0900

    default export main class.

--- src/KXing.ts
+++ src/KXing.ts
@@ -1,7 +1,7 @@
 import {Reader} from "./Reader";
 import QRCodeReader from "./qrcode/QRCodeReader";
 
-export class KXing {
+export default class KXing {
     static getReader(): Reader {
         return new QRCodeReader();
     }
