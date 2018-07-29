d65e29b0859c8fbd915a15a5daf065cdc510681c
TatsuyaYamamoto <yamamoto.tatsuya@fujixerox.co.jp>
2018-07-29 13:25:21 +0900

    fix, very heavy test case

--- src/qrcode/format/__tests__/DataMask.spec.ts
+++ src/qrcode/format/__tests__/DataMask.spec.ts
@@ -1,117 +1,109 @@
-import {
-  DataMask,
-  DataMask000,
-  DataMask001,
-  DataMask010,
-  DataMask011,
-  DataMask100,
-  DataMask111,
-  DataMask101,
-  DataMask110
-} from "../DataMask";
+import { DataMask } from "../DataMask";
 import BitMatrix from "../../../common/BitMatrix";
+
+type IsMasked = (i: number, j: number) => boolean;
 
 describe("DataMask", function() {
   describe("DataMask000", function() {
-    class DataMask000Condition implements MaskCondition {
-      public isMasked(i: number, j: number): boolean {
-        return (i + j) % 2 == 0;
-      }
-    }
     it("should be 0x01", function() {
-      testMaskAcrossDimensions(new DataMask000(), new DataMask000Condition());
+      const isMasked = (i, j) => {
+        return (i + j) % 2 == 0;
+      };
+
+      testMaskAcrossDimensions(0, isMasked);
     });
   });
 
   describe("DataMask001", function() {
-    class DataMask001Condition implements MaskCondition {
-      public isMasked(i: number, _j: number): boolean {
-        return i % 2 == 0;
-      }
-    }
+    const isMasked = (i, _) => {
+      return i % 2 == 0;
+    };
+
     it("should be 0x01", function() {
-      testMaskAcrossDimensions(new DataMask001(), new DataMask001Condition());
+      testMaskAcrossDimensions(1, isMasked);
     });
   });
 
   describe("DataMask010", function() {
-    class DataMask010Condition implements MaskCondition {
-      public isMasked(_i: number, j: number): boolean {
-        return j % 3 == 0;
-      }
-    }
+    const isMasked = (_, j) => {
+      return j % 3 == 0;
+    };
+
     it("should be 0x01", function() {
-      testMaskAcrossDimensions(new DataMask010(), new DataMask010Condition());
+      testMaskAcrossDimensions(2, isMasked);
     });
   });
+
   describe("DataMask011", function() {
-    class DataMask011Condition implements MaskCondition {
-      public isMasked(i: number, j: number): boolean {
-        return (i + j) % 3 == 0;
-      }
-    }
+    const isMasked = (i, j) => {
+      return (i + j) % 3 == 0;
+    };
+
     it("should be 0x01", function() {
-      testMaskAcrossDimensions(new DataMask011(), new DataMask011Condition());
+      testMaskAcrossDimensions(3, isMasked);
     });
   });
 
   // TODO: Can't success this test case......
   describe.skip("DataMask100", function() {
-    class DataMask100Condition implements MaskCondition {
-      public isMasked(i: number, j: number): boolean {
-        return (i / 2 + j / 3) % 2 == 0;
-      }
-    }
+    const isMasked = (i, j) => {
+      return (i / 2 + j / 3) % 2 == 0;
+    };
+
     it("should be 0x01", function() {
-      testMaskAcrossDimensions(new DataMask100(), new DataMask100Condition());
+      testMaskAcrossDimensions(4, isMasked);
     });
   });
+
   describe("DataMask101", function() {
-    class DataMask101Condition implements MaskCondition {
-      public isMasked(i: number, j: number): boolean {
-        return ((i * j) % 2) + ((i * j) % 3) == 0;
-      }
-    }
+    const isMasked = (i, j) => {
+      return ((i * j) % 2) + ((i * j) % 3) == 0;
+    };
+
     it("should be 0x01", function() {
-      testMaskAcrossDimensions(new DataMask101(), new DataMask101Condition());
+      testMaskAcrossDimensions(5, isMasked);
     });
   });
+
   describe("DataMask110", function() {
-    class DataMask110Condition implements MaskCondition {
-      public isMasked(i: number, j: number): boolean {
-        return (((i * j) % 2) + ((i * j) % 3)) % 2 == 0;
-      }
-    }
+    const isMasked = (i, j) => {
+      return (((i * j) % 2) + ((i * j) % 3)) % 2 == 0;
+    };
+
     it("should be 0x01", function() {
-      testMaskAcrossDimensions(new DataMask110(), new DataMask110Condition());
+      testMaskAcrossDimensions(6, isMasked);
     });
   });
 
   describe("DataMask111", function() {
-    class DataMask111Condition implements MaskCondition {
-      public isMasked(i: number, j: number): boolean {
-        return (((i + j) % 2) + ((i * j) % 3)) % 2 == 0;
-      }
-    }
+    const isMasked = (i, j) => {
+      return (((i + j) % 2) + ((i * j) % 3)) % 2 == 0;
+    };
+
     it("should be 0x01", function() {
-      testMaskAcrossDimensions(new DataMask111(), new DataMask111Condition());
+      testMaskAcrossDimensions(7, isMasked);
     });
   });
 });
 
-interface MaskCondition {
-  isMasked(i: number, j: number): boolean;
-}
-
-function testMaskAcrossDimensions(mask: DataMask, condition: any): void {
+function testMaskAcrossDimensions(reference: number, isMasked: IsMasked): void {
+  const mask: DataMask = DataMask.values()[reference];
   for (let version = 1; version <= 40; version++) {
-    const dimension: number = 17 + 4 * version;
+    const dimension = 17 + 4 * version;
+
     const bits: BitMatrix = new BitMatrix(dimension);
     mask.unmaskBitMatrix(bits, dimension);
+
+    const isMaskedList: boolean[] = [];
+    const bitList: boolean[] = [];
+
     for (let i = 0; i < dimension; i++) {
       for (let j = 0; j < dimension; j++) {
-        expect(condition.isMasked(i, j)).toEqual(bits.getBit(j, i));
+        isMaskedList.push(isMasked(i, j));
+        bitList.push(bits.getBit(j, i));
       }
     }
+
+    expect(isMaskedList).toEqual(bitList);
   }
 }
